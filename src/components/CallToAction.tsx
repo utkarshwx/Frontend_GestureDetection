671e5541c07b31fcfd6dbda5304ea43f19116f53
Utkarsh Nigam <webxthedev@gmail.com>
2024-12-03 11:43:15 +0530

    Update CallToAction.tsx

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -12,7 +12,6 @@ export function CallToAction() {
           Get started with GestureAI today and experience unparalleled<br />
           accuracy and ease of integration
         </p>
-        <button className="cta-button">START FREE TRIAL</button>
       </div>
       <div className="cta-background"></div>
     </section>
